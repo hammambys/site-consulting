9eb9d259170a5ef4cfe44abbdcd3c638e801a73d
hammam <hammambenyounes4@gmail.com>
2022-03-14 12:40:49 +0100

    fixed static file issue

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ import { engine } from "express-handlebars";
 import express from "express";
 import "dotenv/config";
 import router from "./routes/router.js";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const port = process.env.PORT || 5000;
 const app = express();
@@ -14,10 +18,14 @@ app.use(express.json());
 
 // Static Files
 app.use(express.static("public"));
+app.use("/css", express.static(__dirname + "/public/css"));
+app.use("/lib", express.static(__dirname + "/public/lib"));
+app.use("/img", express.static(__dirname + "/public/img"));
+app.use("/js", express.static(__dirname + "/public/js"));
 
 // Templating Engine
-app.engine("handlebars", engine());
-app.set("view engine", "handlebars");
+app.engine("hbs", engine());
+app.set("view engine", "hbs");
 app.set("views", "./views");
 
 app.use("/", router);
